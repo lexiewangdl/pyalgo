e405ebe3ed541ef1e78f41c663f0ca520bfbff30
Lexie Wang <travelaroundthemoon@gmail.com>
2023-08-24 19:54:16 -0700

    26. remove duplicates from sorted list

--- two_pointers/remove_duplicates_from_sorted_list.py
+++ two_pointers/remove_duplicates_from_sorted_list.py
@@ -9,18 +9,15 @@ def deleteDuplicates(head):
     if not head or not head.next:
         return head
 
-    p1 = head
-    p2 = head.next
+    p1 = head  # last unique element (p1 is unique and everything to its left is unique)
+    p2 = head.next  # next element to be checked
 
     while p2:
-        if p2.val == p1.val:
-            if not p2.next:  # if p2 is last node in linked list
-                p1.next = None  # delete p2 from linked list
-                return head
-            p2 = p2.next  # p2 is a duplicate, ignore it
-        else:  # found another unique element
+        if p1.val != p2.val:  # found another unique element
             p1.next = p2  # connect p1 and p2 (same as deleting everything in between)
             p1 = p1.next
-            p2 = p2.next
+        elif not p2.next:  # if p2 is last node in linked list
+            p1.next = None  # delete p2 from linked list
+        p2 = p2.next  # otherwise, p2 is a duplicate, ignore it
 
     return head
